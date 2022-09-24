89dba16e60255ca3950be9af608693de0f7a9797
LandyshH <86878681+LandyshH@users.noreply.github.com>
2022-09-24 21:56:03 +0300

    Немного верстки

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
+import {
+    AppBar
+} from "@mui/material";
+import MessageArea from "./components/message-area";
 import './App.css';
 
+
 function App() {
+    const DUMMY_DATA = [
+        {
+            senderId: "perborgen",
+            text: "who'll win?"
+        },
+        {
+            senderId: "janedoe",
+            text: "who'll win?"
+        }
+    ]
+    
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <MessageArea></MessageArea>
+      </div>
   );
 }
 
---
+++ src/client/src/components/message-area.tsx
@@ -0,0 +1,14 @@
+﻿import React from 'react';
+import '../App.css';
+
+class MessageArea extends React.Component {
+   render() {
+       return (
+           <div className="message-area-container">
+               
+           </div>
+       );
+   }
+}
+
+export default MessageArea;
