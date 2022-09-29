42eef2b390dd88a7c718d28985abd935751b7e01
LandyshH <86878681+LandyshH@users.noreply.github.com>
2022-09-29 17:40:22 +0300

    рефакторинг

--- src/client/src/components/custom-text-area.tsx
+++ src/client/src/components/custom-text-area.tsx
@@ -5,14 +5,6 @@ import CustomInput from "./CustomInput";
 
 
 const CustomTextArea = () => {
-
-    const [message, setMessage] = useState({text: ''})
-
-    const addNewPost = (e: any) => {
-        e.preventDefault()
-        setMessage({text: ''})
-    }
-
     return (
             <form className="custom-text-area-form" >
                     <CustomInput type={"text"}></CustomInput>
