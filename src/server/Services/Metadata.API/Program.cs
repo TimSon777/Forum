437bf59206389349c3fe46a26974e423123baf00
TimSon777 <dusalimov.tima@yandex.ru>
2022-12-14 02:28:54 +0300

    fix: add minimal api config to Metadata.API

--- src/server/Services/Metadata.API/Program.cs
+++ src/server/Services/Metadata.API/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 var configuration = builder.Configuration;
 
+services.AddEndpoints();
 services.AddSwaggerGen();
 services.AddEndpointsApiExplorer();
 
